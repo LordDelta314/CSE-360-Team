06c4a89077207a1480da5304aac8253d7c6599e0
Maverick Madison <134321883+Gawdlie@users.noreply.github.com>
2025-09-30 18:06:43 -0700

    Update ViewUserUpdate.java

--- HW1/src/guiUserUpdate/ViewUserUpdate.java
+++ HW1/src/guiUserUpdate/ViewUserUpdate.java
@@ -118,8 +118,10 @@ public class ViewUserUpdate {
 	private static Optional<String> result;		// The result from a pop-up dialog
 
 	protected static Alert invalidPassAlert = new Alert(AlertType.INFORMATION);
+	protected static Alert invalidEmailAlert = new Alert(AlertType.INFORMATION);
 	
 	private static boolean passValid = true;  // Tracks if a password is valid, default set to true 
+	private static boolean emailValid = true; // Tracks if an email is valid, default set to true
 	private static String originalPass = "";  // Variable to keep track of the original password before it is changed, for validation reasons
 	
 	/*-********************************************************************************************
@@ -300,6 +302,10 @@ public class ViewUserUpdate {
 		invalidPassAlert.setTitle("Please enter a valid password");
 		invalidPassAlert.setHeaderText("You must fill in all necessary information for a password (upper, lower, special, number, min length of 8)");
 		invalidPassAlert.setContentText("Correct this issue and try again please.");
+
+		invalidEmailAlert.setTitle("Please enter a valid email");
+		invalidEmailAlert.setHeaderText("You must fill in all necessary information for a email.");
+		invalidEmailAlert.setContentText("Correct this issue and try again please.");
 		
         // USername
         setupLabelUI(label_Username, "Arial", 18, 190, Pos.BASELINE_RIGHT, 5, 100);
@@ -451,14 +457,280 @@ public class ViewUserUpdate {
         setupLabelUI(label_EmailAddress, "Arial", 18, 190, Pos.BASELINE_RIGHT, 5, 400);
         setupLabelUI(label_CurrentEmailAddress, "Arial", 18, 260, Pos.BASELINE_LEFT, 200, 400);
         setupButtonUI(button_UpdateEmailAddress, "Dialog", 18, 275, Pos.CENTER, 500, 393);
-        button_UpdateEmailAddress.setOnAction((event) -> {result = dialogUpdateEmailAddresss.showAndWait();
-    		result.ifPresent(name -> theDatabase.updateEmailAddress(theUser.getUserName(), result.get()));
-    		theDatabase.getUserAccountDetails(theUser.getUserName());
-    		String newEmail = theDatabase.getCurrentEmailAddress();
-           	theUser.setEmailAddress(newEmail);
-        	if (newEmail == null || newEmail.length() < 1)label_CurrentEmailAddress.setText("<none>");
-        	else label_CurrentEmailAddress.setText(newEmail);
- 			});
+        button_UpdateEmailAddress.setOnAction((event) -> {
+        	result = dialogUpdateEmailAddresss.showAndWait();
+        	emailValid = true;
+    		result.ifPresent(name -> {
+    			if(name.equals("")) {
+    				ViewUserUpdate.label_CurrentEmailAddress.setText(theDatabase.getCurrentEmailAddress());
+    				emailValid = false;
+    				ViewUserUpdate.invalidEmailAlert.showAndWait();
+    				return;
+    			}
+    			
+    			String emailAddressErrorMessage = "";	// The error message text
+    			String emailAddressInput = "";		// The input being processed
+    			int emailAddressIndexofError = -1;	// The index where the error was located
+    			int state = 0;						// The current state value
+    			int nextState = 0;					// The next state value
+    			boolean finalState = false;			// Is this state a final state?
+    			String inputLine = "";				// The input line
+    			char currentChar;					// The current character in the line
+    			int currentCharNdx;					// The index of the current character
+    			boolean running;						// The flag that specifies if the FSM is 
+    																// running
+    			int domainPartCounter = 0;			// A domain name may not exceed 63 characters
+    			
+    			state = 0;							// This is the FSM state number
+				inputLine = name;					// Save the reference to the input line as a global
+				currentCharNdx = 0;					// The index of the current character
+
+				// The Finite State Machines continues until the end of the input is reached or at some 
+				// state the current character does not match any valid transition to a next state
+
+				emailAddressInput = name;			// Save a copy of the input
+
+				// Let's ensure there is input
+				currentChar = name.charAt(0);		// The current character from the above indexed position
+
+				// Let's ensure the address is not too long
+				if (name.length() > 255) {
+					emailAddressErrorMessage = "A valid email address must be no more than 255 characters.\n";
+					emailValid = false;
+				}
+				running = true;						// Start the loop
+				System.out.println("\nCurrent Final Input  Next  DomainName\nState   State Char  State  Size");
+
+				// The Finite State Machines continues until the end of the input is reached or at some 
+				// state the current character does not match any valid transition to a next state
+				while (running) {
+					// The switch statement takes the execution to the code for the current state, where
+					// that code sees whether or not the current character is valid to transition to a
+					// next state
+					nextState = -1;						// Default to there is no next state		
+					
+					switch (state) {
+					case 0: 
+						// State 0 has just 1 valid transition.
+						// The current character is must be checked against 62 options. If any are matched
+						// the FSM must go to state 1
+						// The first and the second check for an alphabet character the third a numeric
+						if ((currentChar >= 'A' && currentChar <= 'Z')|| 		// Upper case
+								(currentChar >= 'a' && currentChar <= 'z') ||	// Lower case
+								(currentChar >= '0' && currentChar <= '9')) {	// Digit
+							nextState = 1;
+						}
+										
+						// If it is none of those characters, the FSM halts
+						else { 
+							running = false;
+						}
+						
+						break;				
+						// The execution of this state is finished
+					
+					case 1: 
+						// State 1 has three valid transitions.  
+						
+						// Replace this with the required code
+						if ((currentChar >= 'A' && currentChar <= 'Z')|| 		// Upper case
+								(currentChar >= 'a' && currentChar <= 'z') ||	// Lower case
+								(currentChar >= '0' && currentChar <= '9')) {	// Digit
+							nextState = 1;
+						}
+						
+						// If the next character is a period, FSM transitions to state 0
+						else if (currentChar == '.') {
+							nextState = 0;
+						}
+						
+						// If the next character is @, FSM transitions to state 2
+						else if (currentChar == '@') {
+							nextState = 2;
+						}
+						
+						// If it is none of those characters, the FSM halts
+						else {
+							running = false;
+						}
+						
+						break;
+						// The execution of this state is finished
+									
+					case 2: 
+						// State 2 has one valid transition.
+						
+						// Replace this with the required code
+						if ((currentChar >= 'A' && currentChar <= 'Z')|| 		// Upper case
+								(currentChar >= 'a' && currentChar <= 'z') ||	// Lower case
+								(currentChar >= '0' && currentChar <= '9')) {	// Digit
+							nextState = 3;
+						}
+						
+						// If it is none of those characters, the FSM halts
+						else {
+							running = false;
+						}
+						
+						// The execution of this state is finished
+						break;
+			
+					case 3:
+						// State 3 has three valid transition.
+						
+						// Replace this with the required code
+						if ((currentChar >= 'A' && currentChar <= 'Z')|| 		// Upper case
+								(currentChar >= 'a' && currentChar <= 'z') ||	// Lower case
+								(currentChar >= '0' && currentChar <= '9')) {	// Digit
+							nextState = 3;
+						}
+						
+						// If the next character is a period, the FSM transitions to state 2
+						else if (currentChar == '.') {
+							nextState = 2;
+						}
+						
+						// If the next character is a hyphen, the FSM transitions to state 4
+						else if (currentChar == '-') {
+							nextState = 4;
+						}
+						
+						// If it is none of those characters, the FSM halts
+						else {
+							running = false;
+						}
+						
+						// The execution of this state is finished
+						break;
+
+					case 4: 
+						// State 4 has one valid transition.
+
+						// Replace this with the required code
+						if ((currentChar >= 'A' && currentChar <= 'Z')|| 		// Upper case
+								(currentChar >= 'a' && currentChar <= 'z') ||	// Lower case
+								(currentChar >= '0' && currentChar <= '9')) {	// Digit
+							nextState = 3;
+						}
+						
+						// If it is none of those characters, the FSM halts
+						else {
+							running = false;
+						}
+						// The execution of this state is finished
+						break;
+
+					}
+					
+					if (running) {
+						//displayDebuggingInfo();
+						// When the processing of a state has finished, the FSM proceeds to the next character
+						// in the input and if there is one, it fetches that character and updates the 
+						// currentChar.  If there is no next character the currentChar is set to a blank.
+						
+						currentCharNdx++;
+						if (currentCharNdx < inputLine.length())
+									currentChar = inputLine.charAt(currentCharNdx);
+						else {
+							System.out.println("End of input was found!");
+							currentChar = ' ';
+							running = false;
+						}
+						
+						// Move to the next state
+						state = nextState;
+						nextState = -1;
+					}
+					// Should the FSM get here, the loop starts again
+
+				}
+				// displayDebuggingInfo();
+				
+				System.out.println("The loop has ended.");
+				System.out.println(name);
+
+				emailAddressIndexofError = currentCharNdx;		// Copy the index of the current character;
+				
+				// When the FSM halts, we must determine if the situation is an error or not.  That depends
+				// of the current state of the FSM and whether or not the whole string has been consumed.
+				// This switch directs the execution to separate code for each of the FSM states and that
+				// makes it possible for this code to display a very specific error message to improve the
+				// user experience.
+				switch (state) {
+				case 0:
+					// State 0 is not a final state, so we can return a very specific error message
+					emailAddressIndexofError = currentCharNdx;		// Copy the index of the current character;
+					emailAddressErrorMessage = "May only be alphanumberic.\n";
+					emailValid = false;
+					ViewUserUpdate.label_CurrentEmailAddress.setText(theDatabase.getCurrentEmailAddress());
+	    			ViewUserUpdate.invalidEmailAlert.showAndWait();
+	    			return;
+
+				case 1:
+					// State 1 is not a final state, so we can return a very specific error message
+
+					// Replace this with the required code
+					emailAddressIndexofError = currentCharNdx;
+					emailAddressErrorMessage = "May only use alphanumeric, an '@' symbol, or a period.\n";
+					emailValid = false;
+					ViewUserUpdate.label_CurrentEmailAddress.setText(theDatabase.getCurrentEmailAddress());
+	    			ViewUserUpdate.invalidEmailAlert.showAndWait();
+	    			return;
+					
+				case 2:
+					// State 2 is not a final state, so we can return a very specific error message
+					
+					// Replace this with the required code
+					emailAddressIndexofError = currentCharNdx;
+					emailAddressErrorMessage = "May only use alphanumeric.\n";
+					emailValid = false;
+					ViewUserUpdate.label_CurrentEmailAddress.setText(theDatabase.getCurrentEmailAddress());
+	    			ViewUserUpdate.invalidEmailAlert.showAndWait();
+	    			return;
+					
+				case 3:
+					// State 3 is a Final State, so this is not an error if the input is empty, otherwise
+					// we can return a very specific error message.
+
+					if (currentCharNdx<name.length()) {
+						// If not all of the string has been consumed, we point to the current character
+						// in the input line and specify what that character must be in order to move
+						// forward.
+						emailAddressIndexofError = currentCharNdx;		// Copy the index of the current character;
+						emailAddressErrorMessage = "This must be the end of the input.\n";
+						emailValid = false;
+						ViewUserUpdate.label_CurrentEmailAddress.setText(theDatabase.getCurrentEmailAddress());
+		    			ViewUserUpdate.invalidEmailAlert.showAndWait();
+		    			return;
+					}
+					else 
+					{
+						emailAddressIndexofError = -1;
+						emailAddressErrorMessage = "";
+						emailValid = true;
+						theDatabase.updateEmailAddress(theUser.getUserName(), name);
+		        		String newEmail = theDatabase.getCurrentEmailAddress();
+		        		System.out.println(newEmail);
+		        		theUser.setEmailAddress(newEmail);
+		        		label_CurrentEmailAddress.setText(newEmail);
+		        		break;
+					}
+
+				case 4:
+					// State 4 is not a final state, so we can return a very specific error message. 
+
+					// Replace this with the required code
+					emailAddressIndexofError = currentCharNdx;
+					emailAddressErrorMessage = "May only use alphanumeric.\n";
+					emailValid = false;
+					ViewUserUpdate.label_CurrentEmailAddress.setText(theDatabase.getCurrentEmailAddress());
+	    			ViewUserUpdate.invalidEmailAlert.showAndWait();
+	    			return;
+				default:
+					emailValid = true;
+				}
+    		});
+        });
         
         // Set up the button to proceed to this user's home page
         setupButtonUI(button_ProceedToUserHomePage, "Dialog", 18, 300, 
